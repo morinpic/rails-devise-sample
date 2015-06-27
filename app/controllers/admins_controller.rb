4e650c90e895132d12e8a47cd89e3e8290864a0d
morinpic <morinpic0518@gmail.com>
2015-06-28 06:46:17 +0900

    add Adminモデル新規作成処理追加

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,8 +2,28 @@ class AdminsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @admins = Admin.all
   end
 
   def show
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to admins_path, notice: '新規管理者を作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:email, :password)
   end
 end
